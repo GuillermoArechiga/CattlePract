b6ef5e7597d62fe4a3698d9b9a76b9192993ef3b
GuillermoArechiga <guillermo.arechigaav@gmail.com>
2025-05-07 18:26:10 -0600

    init react, tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+
+  return (
+      <>Insert Components Here</>
+  )
+}
+
+export default App
---
+++ src/components/AddCattle.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddCattle = () => {
+  return (
+    <div>AddCattle</div>
+  )
+}
